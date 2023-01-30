1002ad80a49f575821e4da326dca25b446625b60
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2023-01-30 16:13:37 +0000

    1

--- Assets/Scripts/StartGameButton.cs
+++ Assets/Scripts/StartGameButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonUI : MonoBehaviour
+public class StartGameButton : MonoBehaviour
 {
     [SerializeField] private string newGameLevel = "MainMenu";
 
