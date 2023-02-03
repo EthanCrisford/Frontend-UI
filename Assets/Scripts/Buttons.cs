c710d6ec6d250d8abeac3c78e055fe7969a5ab48
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2023-02-03 09:19:20 +0000

    1

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartGameButton : MonoBehaviour
+public class Buttons : MonoBehaviour
 {
     [SerializeField] private string newGameLevel = "MainMenu";
+    //[SerializeField] private string newGameLevel = "Settings";
 
-    public void NewgameButton()
+    public void StartGameButton()
     {
         SceneManager.LoadScene(newGameLevel);
     }
