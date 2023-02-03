f4162105e10f9cd3b4238e77619a54f47a195540
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2023-02-03 09:53:43 +0000

    1

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -5,11 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-    [SerializeField] private string newGameLevel = "MainMenu";
-    //[SerializeField] private string newGameLevel = "Settings";
+    [SerializeField] private string StartgameButton;
+    [SerializeField] private string SettingsButton;
+    [SerializeField] private string QuitGameButton;
+    [SerializeField] private string BackButton;
+    [SerializeField] private string AudioOptionsButton;
+
 
     public void StartGameButton()
     {
-        SceneManager.LoadScene(newGameLevel);
+        SceneManager.LoadScene(StartgameButton);
+    }
+
+    public void OptionsButton()
+    {
+        SceneManager.LoadScene(SettingsButton);
+    }
+
+    public void QuitButton()
+    {
+        SceneManager.LoadScene(QuitGameButton);
+    }
+
+    public void BackToButton()
+    {
+        SceneManager.LoadScene(BackButton);
+    }
+
+    public void AudoSettingsButton()
+    {
+        SceneManager.LoadScene(AudioOptionsButton);
     }
 }
