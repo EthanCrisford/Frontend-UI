a7e3c4a94bce974f7a037eb0d7b73fb682659de1
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2023-02-03 13:40:07 +0000

    1

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -1,16 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Audio : MonoBehaviour
 {
-    private void Awake()
+ 
+    [SerializeField] Slider volumeSlider;
+    [System.Serializable]
+    public class Sound
+
+    {
+        public string name;
+
+        public AudioClip clip;
+
+        [Range(0f, 1f)]
+        public float volume;
+        [Range(1f, 3f)]
+        public float pitch;
+
+        [HideInInspector]
+        public AudioSource source;
+    }
+
+    void Start()
+    {
+        {
+            if (!PlayerPrefs.HasKey("musicVol"))
+            {
+                PlayerPrefs.SetFloat("musicVol", 1);
+                Load();
+            }
+            else
+            {
+                Load();
+            }
+        }
+    }
+         void Awake()
     {
         GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
         if (musicObj.Length > 1)
         {
             Destroy(this.gameObject);
+            AudioListener.volume = volumeSlider.value;
+
         }
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void ChangeVol()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVol", volumeSlider.value);
+    }
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVol");
     }
 }
--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -5,35 +5,73 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-    [SerializeField] private string StartgameButton;
+    /*
+    [SerializeField] private string MainMenu;
+    [SerializeField] private string StartGame;
     [SerializeField] private string SettingsButton;
     [SerializeField] private string QuitGameButton;
     [SerializeField] private string BackButton;
     [SerializeField] private string AudioOptionsButton;
-
+    [SerializeField] private string AreYouSure;
+    [SerializeField] private string ExitToDesktop;
+    */
 
     public void StartGameButton()
     {
-        SceneManager.LoadScene(StartgameButton);
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void OptionsButton()
     {
-        SceneManager.LoadScene(SettingsButton);
+        SceneManager.LoadScene("Settings");
     }
 
-    public void QuitButton()
+    public void BackToStartGameButton()
     {
-        SceneManager.LoadScene(QuitGameButton);
+        SceneManager.LoadScene("StartGame");
     }
 
-    public void BackToButton()
+    public void BackToSettingsButton()
     {
-        SceneManager.LoadScene(BackButton);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void BackToMainMenuButton()
+    {
+        SceneManager.LoadScene("StartGame");
     }
 
     public void AudoSettingsButton()
     {
-        SceneManager.LoadScene(AudioOptionsButton);
+        SceneManager.LoadScene("AudioSettings");
+    }
+
+    public void AreYouSureNo()
+    {
+        SceneManager.LoadScene("StartGame");
+
+    }
+
+    public void AreYouSureYes()
+    {
+        Application.Quit();
+    }
+
+
+    public void AreYouSure()
+    {
+        SceneManager.LoadScene("AreYouSure");
+    }
+
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("ExitToDesktop");
     }
 }
